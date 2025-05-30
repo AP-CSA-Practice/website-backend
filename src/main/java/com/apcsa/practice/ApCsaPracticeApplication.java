c65b998238760fbec5f3541c0a90eb75e1d704d8
LarryLin35 <124746278+LarryLin35@users.noreply.github.com>
2025-05-31 00:07:08 +0800

    feat(spring-boot): Initailse the basic code

---
+++ src/main/java/com/apcsa/practice/ApCsaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.apcsa.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApCsaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApCsaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apcsa/practice/ApCsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apcsa.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApCsaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
